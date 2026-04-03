b14a670b4af65c4d7b1c06302e6dc43a0065b872
Nikoll Borja <est.nikoll.borja@unimilitar.edu.co>
2026-04-03 15:17:14 -0500

    Implementación del algoritmo de Bresenham: cálculo de dx, dy, dirección de pasos y variable de error para trazar la línea

--- bresenham.js
+++ bresenham.js
@@ -4,6 +4,51 @@
  * @param {number} y 
  */
 function plot(x, y) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(x * escala, y * escala, escala, escala);
+	ctx.fillStyle = "black";
+	ctx.fillRect(x * escala, y * escala, escala, escala);
+}
+/**
+ * Algoritmo de Bresenham con almacenamiento de pasos
+ */
+function bresenham(x0, y0, x1, y1, plot) {
+
+	let dx = Math.abs(x1 - x0);
+	let dy = Math.abs(y1 - y0);
+
+	let sx = (x0 < x1) ? 1 : -1;
+	let sy = (y0 < y1) ? 1 : -1;
+
+	let err = dx - dy;
+
+	let pasos = [];
+	let paso = 0;
+
+	while (true) {
+
+		plot(x0, y0);
+
+		let e2 = 2 * err;
+
+		pasos.push({
+			paso: paso++,
+			x: x0,
+			y: y0,
+			err: err,
+			e2: e2
+		});
+
+		if (x0 === x1 && y0 === y1) break;
+
+		if (e2 > -dy) {
+			err -= dy;
+			x0 += sx;
+		}
+
+		if (e2 < dx) {
+			err += dx;
+			y0 += sy;
+		}
+	}
+
+	return pasos;
 }
